a7580dfeab7abd9cf11257933a156d2055bff0e8
ampiduxmoe <ampiduxmoe@gmail.com>
2021-10-14 01:39:18 +0500

    оформил страницу задания

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.where(contest_id: params[:contest_id])
+    @task_solved = Hash.new
+    @tasks.map do |task|
+      result = Result.for(current_user, task)&.first&.user_query
+      @task_solved[task] = if result.present? then true else false end
+    end
   end
 
   def show
@@ -33,12 +38,11 @@ class TasksController < ApplicationController
     begin
       results = ActiveRecord::Base.connection.exec_query(sql)
     rescue ActiveRecord::StatementInvalid
-      return 'Неправильный запрос!'
+      return nil
     end
     if results.present?
-      results
-    else
-      nil
+      return results
+    nil
     end
   end
 end
