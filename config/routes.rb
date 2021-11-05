6c9ee71f774fdf28be6599071b99b589065e49e7
ampiduxmoe <ampiduxmoe@gmail.com>
2021-11-05 20:35:38 +0500

    стили и роуты для будущих страниц

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   resources :contests do
     resources :tasks
   end
-
+  resources :exercise_categories do
+    resources :tasks
+  end
+  resources :users do
+  end
+  get 'about_us', to: 'about_us#index'
+  get 'leaderboard', to: 'leaderboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
